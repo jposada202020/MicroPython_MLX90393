9abbf02a1ecd2e78f5429706b2ca3fc5721b1855
jposada202020 <jquintana202020@gmail.com>
2023-07-13 18:39:26 -0400

    update

--- examples/micropython_mlx90393_simpletest.py
+++ examples/micropython_mlx90393_simpletest.py
@@ -2,14 +2,13 @@
 
 import time
 from machine import Pin, I2C
-import mlx90393
+from micropython_mlx90393 import mlx90393
 
 i2c = I2C(sda=Pin(8), scl=Pin(9))  # Correct I2C pins for UM FeatherS2
 mlx = mlx90393.MLX90393(i2c)
 
 while True:
     magx, magy, magz = mlx.magnetic
-    print("X: {} uT".format(magx))
-    print("Y: {} uT".format(magy))
-    print("Z: {} uT".format(magz))
+    print(f"X: {magx} uT, Y: {magy} uT, Z: {magz} uT")
+    print()
     time.sleep(1)
